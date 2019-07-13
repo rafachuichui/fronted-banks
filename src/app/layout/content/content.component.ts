b60a079bf388e9b14b45521600d2324787e7812d
rafachuichui <rmiguezr@gmail.com>
2019-07-13 10:54:23 +0200

    content

---
+++ src/app/layout/content/content.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.scss']
+})
+export class ContentComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  gotoCadastroClientes() {
+    this.router.navigate(['registro-clientes']);
+  }
+
+}
