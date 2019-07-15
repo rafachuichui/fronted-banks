9bd7f95ef2916521ef9c7c89ec73b132002fe243
rafachuichui <rmiguezr@gmail.com>
2019-07-15 13:33:57 +0200

    actualizado app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material';
+import { SingletonRouterService } from 'src/services/singleton-Router.service';
+
 
 @Component({
   selector: 'app-root',
@@ -8,12 +10,19 @@ import { MatDialog } from '@angular/material';
 })
 export class AppComponent {
   title = 'bank';
+  isShow: boolean;
+  data = false;
 
 
-  constructor(public dialog: MatDialog) {
+  constructor(
+    public dialog: MatDialog,
+    private singletonService: SingletonRouterService) {
+  }
 
-  };
-
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnInit() {
+    this.isShow = this.singletonService.getCredentials();
+  }
   openDialog() {
     const dialogRef = this.dialog.open(AppComponent, {
       height: '350px'
