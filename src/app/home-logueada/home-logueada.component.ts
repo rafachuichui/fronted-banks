9e72855e805a60e5af8282220e919a667cf7edce
rafachuichui <rmiguezr@gmail.com>
2019-07-16 12:16:55 +0200

    home-logueada

--- src/app/home-logueada/home-logueada.component.ts
+++ src/app/home-logueada/home-logueada.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ElementRef } from '@angular/core';
 @Component({
   selector: 'app-home-logueada',
-  template: `
-    <p>home-logueda works!</p>
-  `,
+  templateUrl: './home-logueada.component.html',
   styleUrls: ['./home-logueada.component.scss']
 })
-export class HomeLogueadaComponent implements OnInit {
 
-  constructor() { }
+export class HomeLogueadaComponent implements OnInit {
+  getRegistro;
+  nameCliente;
+
+  constructor(private el: ElementRef) { }
 
   ngOnInit() {
+    console.log(this.el.nativeElement);
+    this.getRegistro = JSON.parse(localStorage.getItem('registro'));
+    this.nameCliente = this.getRegistro['nombre'];
+    console.log(this.nameCliente);
   }
-
 }
+
+
