2549168a87b9952d04c6cc04d1e8e51973a390bd
rafachuichui <rmiguezr@gmail.com>
2019-07-18 13:32:51 +0200

    animation dont work yet

--- src/app/layout/content/content.component.ts
+++ src/app/layout/content/content.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 //typed.js es una libreria de animacion de JS
-//import * as Typed from 'typed.js';
+import * as Typed from 'typed.js';
+
 
 @Component({
   selector: 'app-content',
@@ -13,22 +14,26 @@ export class ContentComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
-
-    // const animationsWords = {
-    //   stringsElement: '#typed-strings',
-    //   strings: ['Esto es Banks of Banks amigos', ' Pide la portabilidad y ven a ser Bankinhg desde hoy mismo', 'Innovavor digital y seguro'],
-    //   typeSpeed: 100,
-    //   backSpeed: 100,
-    //   backDelay: 200,
-    //   smartBackspace: true,
-    //   fadeOut: true,
-    //   showCursor: false,
-    //   startDelay: 1000,
-    //   loop: true
-    // };
+    const animationsWords = {
+      stringsElement: '#typed-strings',
+      strings: ['Esto es Banks of Banks amigos', ' Cambiate a nosotros y empieza a ser Banking desde hoy mismo', 'Innovador digital y seguro'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 200,
+      smartBackspace: true,
+      fadeOut: true,
+      showCursor: false,
+      startDelay: 1000,
+      loop: true
+    };
 
     //const typed = new Typed('.typing-element', animationsWords);
+
+
+
   }
+
+
   gotoRegistroClientes() {
     this.router.navigate(['registro-clientes']);
   }
